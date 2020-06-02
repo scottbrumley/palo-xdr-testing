402784e02cdfbde4cd0a83ab03a2afd26d4168b3
scottbrumley <scott@nsisecure.com>
2020-06-02 15:54:27 -0400

    Adding Linux Mitre T1027 techniques

--- atomic-test/all-linux-test.sh
+++ atomic-test/all-linux-test.sh
@@ -5,6 +5,7 @@ sudo /opt/traps/bin/cytool checkin
 
 echo "Running Defense Evasion Attacks"
 /vagrant/atomic-test/linux/defense-evasion/atomic-T1009.sh
+/vagrant/atomic-test/linux/defense-evasion/atomic-T1027.sh
 
 echo "Running Impact Attacks"
 /vagrant/atomic-test/linux/impact/atomic-T1496.sh
@@ -12,3 +13,4 @@ echo "Running Impact Attacks"
 echo "Running Persistence Attacks"
 /vagrant/atomic-test/linux/persistence/atomic-T1156.sh
 /vagrant/atomic-test/linux/persistence/atomic-T1158.sh
+
